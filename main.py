2ef69ab40ce6f48f1757adc46911d8fae5de918a
emma.kneitinger <emma.kneitinger@rwth-acchen.de>
2024-04-30 12:42:32 +0200

    comment out main.py not needed rn

--- main.py
+++ main.py
@@ -1,7 +1,34 @@
-import cv2
+""" import cv2 as cv
+import sys
+import matplotlib.pyplot as plt
+ 
+image_path = 'input_image.jpg'
 
-imagePath = './photo.png'
+img = cv.imread(image_path)
 
-img = cv2.imread(imagePath)
+print("image shape:", img.shape)
 
-print(img.shape)
+gray_image = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+print("gray image shape:", gray_image.shape)
+
+face_classifier = cv.CascadeClassifier(
+    cv.data.haarcascades + "haarcascade_frontalface_default.xml"
+)
+
+face = face_classifier.detectMultiScale(
+    gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(40, 40)
+)
+
+for (x, y, w, h) in face:
+    cv.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 4)
+
+img_rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+
+plt.figure(figsize=(20,10))
+plt.imshow(img_rgb)
+plt.axis('off')
+
+plt.show()
+
+print("end") """
