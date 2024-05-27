bdaf10328754b94ed8af91708c6105d35a388219
emma.kneitinger <emma.kneitinger@rwth-acchen.de>
2024-05-27 13:41:14 +0200

    comment

--- build-run-docker.sh
+++ build-run-docker.sh
@@ -2,6 +2,7 @@
 
 PROJECT_DIR="/Users/ek/Documents/RWTH/7_SS24/BA/fer-in-dev"
 
+# cd to project directory
 if [ "$(pwd)" != "$PROJECT_DIR" ]; then
     echo "Navigating to project directory: $PROJECT_DIR"
     cd "$PROJECT_DIR" || { echo "Failed to navigate to project directory"; exit 1; }
@@ -9,6 +10,7 @@ else
     echo "Already in the project directory"
 fi
 
+# build container
 IMAGE_NAME="fer-in-dev"
 echo "Building Docker image: $IMAGE_NAME"
 docker build -t "$IMAGE_NAME" .
@@ -18,12 +20,12 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-# Run the Docker container
+# run docker container
 CONTAINER_NAME="fer-in-dev"
 echo "Running Docker container: $CONTAINER_NAME"
 docker run -p 8888:8888 --name "$CONTAINER_NAME" "$IMAGE_NAME"
 
-# Check if the container started successfully
+# check if container was started
 if [ $? -ne 0 ]; then
     echo "Failed to run Docker container"
     exit 1
