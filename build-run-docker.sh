8f595c6a5ded8c2824099d4dd3d64d6907306f6b
emma.kneitinger <emma.kneitinger@rwth-acchen.de>
2024-05-27 11:08:30 +0200

    add docker script

---
+++ build-run-docker.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+PROJECT_DIR="/Users/ek/Documents/RWTH/7_SS24/BA/fer-in-dev"
+
+if [ "$(pwd)" != "$PROJECT_DIR" ]; then
+    echo "Navigating to project directory: $PROJECT_DIR"
+    cd "$PROJECT_DIR" || { echo "Failed to navigate to project directory"; exit 1; }
+else
+    echo "Already in the project directory"
+fi
+
+IMAGE_NAME="fer-in-dev"
+echo "Building Docker image: $IMAGE_NAME"
+docker build -t "$IMAGE_NAME" .
+
+if [ $? -ne 0 ]; then
+    echo "Docker build failed"
+    exit 1
+fi
+
+# Run the Docker container
+CONTAINER_NAME="fer-in-dev"
+echo "Running Docker container: $CONTAINER_NAME"
+docker run -p 8888:8888 --name "$CONTAINER_NAME" "$IMAGE_NAME"
+
+# Check if the container started successfully
+if [ $? -ne 0 ]; then
+    echo "Failed to run Docker container"
+    exit 1
+fi
+
+echo "Docker container is running. Access Jupyter Notebook at http://localhost:8888"
