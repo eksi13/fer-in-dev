5d2419763077da1d324778acabb5a09d8b6c32e1
emma.kneitinger <emma.kneitinger@rwth-acchen.de>
2024-05-31 11:58:56 +0200

    add scripts

--- scripts/run-docker.sh
+++ scripts/run-docker.sh
@@ -4,4 +4,23 @@ CONTAINER_NAME="fer-in-dev"
 HOST_VOLUME_DIR="/Users/ek/Documents/RWTH/7_SS24/BA/fer-in-dev/data"
 CONTAINER_VOLUME_DIR="/project/data"
 
+check_docker_daemon() {
+    docker info > /dev/null 2>&1
+    if [ $? -ne 0 ]; then
+        echo "Docker daemon is not running. Attempting to start it..."
+
+        # start docker desktop
+        open --background -a Docker
+        echo "Waiting for Docker to start..."
+        while ! docker info > /dev/null 2>&1; do
+            sleep 1
+        done
+
+        echo "Docker daemon started successfully."
+    fi
+}
+
+
+check_docker_daemon
+
 docker run -p 8888:8888 --name "$CONTAINER_NAME" -v "$HOST_VOLUME_DIR:$CONTAINER_VOLUME_DIR" -it "$IMAGE_NAME"
